1011a9ab39187ad83cbc891cbe60644cdcc5ae75
혜민 <97522726+heymin2@users.noreply.github.com>
2023-05-09 14:21:20 +0900

    [ADD] color.js 파일 생성

---
+++ color.js
@@ -0,0 +1,13 @@
+export const WHITE = '#ffffff';
+export const BLACK = '#000000';
+
+export const PRIMARY = {
+  DARK: '#0E56AA',
+  DEFAULT: '#3E78BB',
+  LIGHT: '#B7CDE6',
+};
+
+export const SBTN = {
+  DEFAULT: '#DBE6F2',
+  DARK: '#B3BECA',
+};
