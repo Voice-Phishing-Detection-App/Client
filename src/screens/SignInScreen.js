38c10b472c425cf4731da94417558590027697d5
혜민 <97522726+heymin2@users.noreply.github.com>
2023-05-11 14:41:27 +0900

    [FEAT] 로그인에 아이디찾기, 비밀번호찾기, 회원가입 연결

--- src/screens/SignInScreen.js
+++ src/screens/SignInScreen.js
@@ -3,8 +3,9 @@ import Button from '../components/Button';
 import TextInput, { IconNames, ReturnKeyTypes } from '../components/TextInput';
 import { useState, useRef, useEffect } from 'react';
 import { PRIMARY } from '../../color';
+import PropTypes from 'prop-types';
 
-const SignInScreen = () => {
+const SignInScreen = ({ navigation }) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const passwordRef = useRef(null);
@@ -21,10 +22,9 @@ const SignInScreen = () => {
       setIsLoading(true);
       try {
         const data = await signIn(email, password);
-        console.log(data);
         setUser(data);
       } catch (e) {
-        Alert.alert('SignIn Failed', e, [
+        Alert.alert('로그인 실패', e, [
           {
             text: 'Ok',
             onPress: () => setIsLoading(false),
@@ -37,84 +37,99 @@ const SignInScreen = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>피노키오</Text>
-      <TextInput
-        value={email}
-        onChangeText={(text) => setEmail(text.trim())}
-        title={'아이디'}
-        placeholder=""
-        returnKeyType={ReturnKeyTypes.NEXT}
-        iconName={IconNames.EMAIL}
-        onSubmitEditing={() => passwordRef.current.focus()}
-      />
-      <TextInput
-        ref={passwordRef}
-        value={password}
-        onChangeText={(text) => setPassword(text.trim())}
-        title={'비밀번호'}
-        placeholder=""
-        secureTextEntry
-        iconName={IconNames.PASSWORD}
-        onSubmitEditing={onSubmit}
-      />
-      <View style={styles.buttonContainer}>
-        <Button
-          title={'SIGNIN'}
-          onPress={onSubmit}
-          disabled={disabled}
-          isLoading={isLoading}
+      <View style={styles.view}>
+        <TextInput
+          value={email}
+          onChangeText={(text) => setEmail(text.trim())}
+          title={'아이디'}
+          placeholder=""
+          returnKeyType={ReturnKeyTypes.NEXT}
+          iconName={IconNames.EMAIL}
+          onSubmitEditing={() => passwordRef.current.focus()}
+        />
+        <TextInput
+          ref={passwordRef}
+          value={password}
+          onChangeText={(text) => setPassword(text.trim())}
+          title={'비밀번호'}
+          placeholder=""
+          secureTextEntry
+          iconName={IconNames.PASSWORD}
+          onSubmitEditing={onSubmit}
         />
       </View>
-      <View style={{ padding: 10, flexDirection: 'row', alignItems: 'center' }}>
-        <View style={styles.line} />
-        <View>
-          <Text style={{ width: 40, textAlign: 'center' }}>또는</Text>
+      <View style={styles.view2}>
+        <View style={styles.buttonContainer}>
+          <Button
+            title={'로그인'}
+            onPress={onSubmit}
+            disabled={disabled}
+            isLoading={isLoading}
+          />
         </View>
-        <View style={styles.line} />
-      </View>
-      <View style={styles.buttonContainer}>
-        <Button
-          title={'카카오 로그인'}
-          onPress={onSubmit}
-          disabled={disabled}
-          isLoading={isLoading}
-        />
-      </View>
-      <View style={styles.buttonContainer}>
-        <Button
-          title={'네이버 로그인'}
-          onPress={onSubmit}
-          disabled={disabled}
-          isLoading={isLoading}
-        />
-      </View>
-      <View
-        style={{
-          flexDirection: 'row',
-          alignItems: 'center',
-          fontSize: 5,
-        }}
-      >
-        <Text>아이디찾기</Text>
-        <Text> | </Text>
-        <Text>비밀번호찾기</Text>
-        <Text> | </Text>
-        <Text>회원가입</Text>
+        <View
+          style={{ padding: 10, flexDirection: 'row', alignItems: 'center' }}
+        >
+          <View style={styles.line} />
+          <View>
+            <Text style={{ width: 40, textAlign: 'center' }}>또는</Text>
+          </View>
+          <View style={styles.line} />
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button
+            title={'카카오 로그인'}
+            onPress={onSubmit}
+            disabled={disabled}
+            isLoading={isLoading}
+          />
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button
+            title={'네이버 로그인'}
+            onPress={onSubmit}
+            disabled={disabled}
+            isLoading={isLoading}
+          />
+        </View>
+        <View style={styles.textContainer}>
+          <Text onPress={() => navigation.push('FindId')}>아이디찾기</Text>
+          <Text> | </Text>
+          <Text onPress={() => navigation.push('FindPass')}>비밀번호찾기</Text>
+          <Text> | </Text>
+          <Text onPress={() => navigation.push('SignUp')}>회원가입</Text>
+        </View>
       </View>
     </View>
   );
 };
+
+// SignInScreen.propTypes = {
+//   navigation: PropTypes.object,
+// };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    width: 250,
+  },
+  view: {
+    width: '70%',
+  },
+  view2: {
+    width: '65%',
   },
   buttonContainer: {
-    width: '100%',
-    padding: 10,
+    padding: 5,
     marginTop: 10,
+  },
+  textContainer: {
+    padding: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+    fontSize: 5,
+    justifyContent: 'space-evenly',
   },
   text: {
     fontSize: 25,
@@ -122,6 +137,7 @@ const styles = StyleSheet.create({
     color: PRIMARY.DARK,
     fontWeight: 'bold',
   },
+
   line: { flex: 1, height: 1, backgroundColor: 'black' },
 });
 
