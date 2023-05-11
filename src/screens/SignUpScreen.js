06fe1350df744a88aa0f7588bee64d8ffa7a1e6e
혜민 <97522726+heymin2@users.noreply.github.com>
2023-05-11 21:59:10 +0900

    [CHORE] 필요없는 코드 삭제

--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -41,7 +41,6 @@ const SignUpScreen = ({ navigation }) => {
           value={name}
           onChangeText={(text) => setName(text.trim())}
           title={'이름'}
-          placeholder=""
           returnKeyType={ReturnKeyTypes.NEXT}
           onSubmitEditing={() => emailRef.current.focus()}
         />
@@ -50,7 +49,6 @@ const SignUpScreen = ({ navigation }) => {
           value={email}
           onChangeText={(text) => setEmail(text.trim())}
           title={'아이디'}
-          placeholder=""
           returnKeyType={ReturnKeyTypes.NEXT}
           onSubmitEditing={() => passwordRef.current.focus()}
         />
@@ -59,7 +57,6 @@ const SignUpScreen = ({ navigation }) => {
           value={password}
           onChangeText={(text) => setPassword(text.trim())}
           title={'비밀번호'}
-          placeholder=""
           returnKeyType={ReturnKeyTypes.DONE}
         />
       </View>
