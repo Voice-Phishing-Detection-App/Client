1f9daaa3e5cb05fa5efb12d7909e74b9f4b1c2ed
moonyeju <moonyeju8510@gmail.com>
2023-05-22 22:37:01 +0900

    [ADD] centerlist update

--- src/screens/CenterListScreen.js
+++ src/screens/CenterListScreen.js
@@ -1,22 +1,48 @@
-import { StyleSheet, Text, View } from 'react-native';
-import PropTypes from 'prop-types';
+import { FlatList, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import ListItem from '../components/ListItem';
+import { WHITE } from '../../color';
 
-const CenterListScreen = () => {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>CenterListScreen</Text>
-    </View>
-  );
-};
-CenterListScreen.propTypes = {};
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  title: {
-    fontSize: 30,
   },
 });
+const CenterListScreen = () => {
+  const List = [
+    {
+      id: 1,
+      task: ['예주 은행', '010-0000-0000'],
+      isDone: false,
+    },
+    {
+      id: 2,
+      task: ['예주 은행', '010-0000-0000'],
+      isDone: true,
+    },
+    {
+      id: 3,
+      task: ['예주 은행', '010-0000-0000'],
+      isDone: false,
+    },
+    {
+      id: 4,
+      task: ['예주 은행', '010-0000-0000'],
+      isDone: true,
+    },
+  ];
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={List}
+        renderItem={({ item }) => <ListItem item={item} />}
+        windowSize={5}
+        // ListHeaderComponent={() => <View style={{ height: 10 }}></View>}
+        ListHeaderComponent={View}
+        ListHeaderComponentStyle={{ height: 10 }}
+      />
+    </SafeAreaView>
+  );
+};
+
 export default CenterListScreen;
