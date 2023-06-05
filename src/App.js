09f0255ba29ae9605538f801d65cc2635b1b9cb1
혜민 <97522726+heymin2@users.noreply.github.com>
2023-06-05 12:57:50 +0900

    [CHORE] App.js 원상태로 변경

--- src/App.js
+++ src/App.js
@@ -3,23 +3,17 @@ import { UserProvider } from './contexts/UserContext';
 import Navigation from './navigations';
 import AuthStack from './navigations/AuthStack';
 import { NavigationContainer } from '@react-navigation/native';
-import AgreeScreen from './screens/AgreeScreen';
-import Invoice from './test/Invoice';
-import SpeechToText from './test/SpeechToText';
 
 const App = () => {
   return (
-    // <UserProvider>
-    //   <StatusBar style="dark" />
-    //   <Navigation />
-    // </UserProvider>
-    // <NavigationContainer>
+    <UserProvider>
+      <StatusBar style="dark" />
+      <Navigation />
+    </UserProvider>
+    //  <NavigationContainer>
     //   <StatusBar style="auto" />
     //   <AuthStack />
     // </NavigationContainer>
-    <>
-      <SpeechToText />
-    </>
   );
 };
 
